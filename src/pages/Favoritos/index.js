0ccca1f70f8b177860baea4e898318816510b08e
luan santos <aluncard6@gmail.com>
2022-08-16 20:18:54 -0300

    criando pagina favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,8 +1,44 @@
+import { useEffect, useState } from 'react'
+import {Link} from 'react-router-dom'
 import './favoritos.css'
+
+
 function Favoritos(){
+
+    const[filmes,setFilms] = useState()
+    const[load,setLoad] = useState(true)
+
+    useEffect(()=>{
+        const minhaLista=localStorage.getItem('@streemfilms')
+        setFilms(JSON.parse(minhaLista)||[])
+        setLoad(false)
+    },[])
+
+    if(load){
+        return(
+            <div>
+                <h1>loading</h1>
+            </div>
+        )
+    }
+
     return(
-        <div>
-            <h1>tela favoritos</h1>
+        <div className='meus-filmes'>
+            <h1>Meus filmes</h1>
+
+            <ul>
+                {filmes.map((el,i)=>{
+                    return(
+                        <li key={el.id}>
+                            <span>{el.title}</span>
+                            <div>
+                               <Link to={`/filme/${el.id}`}>Ver detalhes</Link>
+                               <button className='excluir' >Excluir</button>
+                            </div>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
