64f1561617e529a8fdf22d17070c73debb3a3a07
luan santos <aluncard6@gmail.com>
2022-08-15 21:55:22 -0300

    configurando api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,43 @@
+import { useEffect, useState } from 'react'
+import api from '../../services/api'
+
+
 function Home(){
+
+    const [filmes, setFilmes]=useState()
+
+    useEffect(()=>{
+        async function loadFilmes(){
+            const response = await api.get("/movie/now_playing",{
+                params:{
+                    api_key:'002208c53c04490b2efe64ff7edc9f1f',
+                    language:'pt-BR',
+                    page:1,
+                }
+                 
+            })
+
+            setFilmes(response.data.results.slice(0,10))
+        }
+        loadFilmes()
+    },[])
+
+
+
     return(
-        <div>Home</div>
+        <div className='container'>
+            <div className='lista-filmes'>
+                {
+                    filmes.map((el,i)=>{
+                        return(
+                            <article key={el.id}><strong>{el.title}</strong></article>
+                        )
+                        
+                    })
+                    
+                }
+            </div>
+        </div>
     )
 }
 
